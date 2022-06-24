cb1368c0ba3d3c65d8cf7663216330770bf7ea96
fabriciomarote <fabrii.cai93@gmail.com>
2022-06-24 00:17:58 -0300

    se modifica el componente MainPage

--- tp-integrador/src/components/MainPage.jsx
+++ tp-integrador/src/components/MainPage.jsx
@@ -1,18 +1,30 @@
-import img from '../juego-logo.png';
+import { useState } from 'react';
+import img2 from '../juego.png';
+import ModalRules from "./Modal"
+import '../styles/MainPage.css';
 
 
 const MainPage = () => {
 
+    const [modelState, setModelState] = useState(false);
+
     return (
         <>
+        <div className='modalOpened' >
+            {modelState  && <ModalRules/>}
+        </div>
           <div className='mainPage-container'>
-            <div className='title'>
-            Piedra, Papel, Tijera, Lagarto o Spock
-            </div>
-            <div className='image-juego'>
-                <img src={img} alt="imagen"/>
-            </div>
-            <div className='buttons'>
+            <div className='box-container'>
+                <div className='title'>
+                Piedra, Papel, Tijera, Lagarto o Spock
+                </div>
+                <div className='image-juego'>
+                    <img className='imagen' src={img2} alt="imagen"/>
+                </div>
+                <div className='buttons'>
+                <button type="submit" className="btn-btn btn-info btn-mp">Jugar!</button>
+                <button onClick={() => {setModelState(true)}} className="btn-btn btn-info btn-mp">Reglas</button>
+                </div>
             </div>
           </div>
         </>
