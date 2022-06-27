46ba8fbd732d19f357f344ffa618911488e7e552
fabriciomarote <fabrii.cai93@gmail.com>
2022-06-27 12:36:16 -0300

    Se modifica y cambia estilo de componente MainPage

--- tp-integrador/src/components/MainPage.jsx
+++ tp-integrador/src/components/MainPage.jsx
@@ -1,6 +1,11 @@
 import { useContext } from 'react';
 import img from '../images/juego.png';
 import Modal from "./Modal"
+import imgRock from '../images/rock-user.jpeg';
+import imgPaper from '../images/paper-user.jpeg';
+import imgScissors from '../images/scissors-user.jpeg';
+import imgLizard from '../images/lizard-user.jpeg';
+import imgSpock from '../images/spock-user.jpeg';
 import '../styles/MainPage.css';
 import { Context } from './Context';
 import { useNavigate } from "react-router-dom";
@@ -17,8 +22,18 @@ const MainPage = () => {
               <div className='title'>
                 Piedra Papel Tijera Lagarto Spock
               </div>
-              <div className='image-juego'>
-                <img className='imagen' src={img} alt="imagen"/>
+              <div className='images-game'>
+                <div className='box-top-images'>
+                  <img className='img-mp' src={imgRock} alt="imagen"/>
+                  <img className='img-mp' src={imgPaper} alt="imagen"/>
+                </div>
+                <div className='box-medium-images'>
+                  <img className='img-mp' src={imgScissors} alt="imagen"/>
+                </div>
+                <div className='box-bottom-images'>
+                  <img className='img-mp' src={imgLizard} alt="imagen"/>
+                  <img className='img-mp' src={imgSpock} alt="imagen"/>
+                </div>
               </div>
               <div className='buttons'>
                 <button type="submit" onClick={goPlayGame} className="btn-btn btn-info btn-mp">Jugar!</button>
