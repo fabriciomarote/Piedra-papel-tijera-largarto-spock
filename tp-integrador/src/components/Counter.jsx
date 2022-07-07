7fe47e9c2d2429794cb32a487af5040ead1de0ef
fabriciomarote <fabrii.cai93@gmail.com>
2022-07-07 10:17:20 -0300

    Se crea el componente Counter que contiene el contador de partida

---
+++ tp-integrador/src/components/Counter.jsx
@@ -0,0 +1,29 @@
+import '../styles/Counter.css';
+
+const Counter = (props) => {
+
+    const { player1, player2 } = props
+
+    return (
+        <>
+            <div className='counter-container'>
+                <div className='counter'>
+                    <div className='counter-top'> 
+                        <p className='title-counter'>Contador de Partida</p>
+                    </div>
+                    <div className='counter-bottom'> 
+                        <div className='counter-left'> 
+                            <p className='point'>{player1}</p>
+                        </div>
+                        <div className='counter-right'> 
+                            <p className='point'>{player2}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default Counter;
