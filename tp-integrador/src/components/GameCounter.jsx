899d0bdcd9e7ecd926e4e2d9bbce7cef1018a7a5
fabriciomarote <fabrii.cai93@gmail.com>
2022-07-07 10:17:54 -0300

    Se crea el componente GameCounter que contiene el contador total del juego

---
+++ tp-integrador/src/components/GameCounter.jsx
@@ -0,0 +1,31 @@
+import '../styles/GameCounter.css';
+
+const GameCounter = (props) => {
+
+    const { player1, player2, namePlayer1, namePlayer2 } = props
+
+    return (
+        <>
+            <div className='counter-total-container'>
+                <div className='counter-total'>
+                    <div className='counter-top'> 
+                        <p className='title-counter-total'>Contador Total</p>
+                    </div>
+                    <div className='counter-bottom'> 
+                        <div className='counter-total-left'> 
+                            <p className='player'>{namePlayer1}</p>
+                            <p className='point-total'>{player1}</p>
+                        </div>
+                        <div className='counter-total-right'> 
+                            <p className='point-total'>{player2}</p>
+                            <p className='player'>{namePlayer2}</p>
+                        </div>
+                    </div>   
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default GameCounter;
