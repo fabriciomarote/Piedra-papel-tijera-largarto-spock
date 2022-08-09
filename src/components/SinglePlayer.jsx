a36f4ecaf23539d79e06479621187c9a145d32b1
fabriciomarote <fabrii.cai93@gmail.com>
2022-08-09 14:02:41 -0300

    se agrega sonido en los componentes SinglePlayer y MultiPlayer

--- src/components/SinglePlayer.jsx
+++ src/components/SinglePlayer.jsx
@@ -3,6 +3,10 @@ import { useNavigate } from "react-router-dom";
 import { selections } from './Selections';
 import win from '../media/winner.png';
 import lose from '../media/loser.png';
+import Ganador from '../media/ganador.mp3'
+import Perdedor from '../media/derrota.mp3'
+import Empate from '../media/empate.mp3'
+import useSound from 'use-sound';
 import Counter from './Counter';
 import GameCounter from './GameCounter';
 import Footer from './Footer';
@@ -17,6 +21,10 @@ const SinglePlayer = () => {
     const [counterComp, setCounterComp] = useState(0);
     const [userSelection, setUserSelection] = useState(null);
     const [compSelection, setCompSelection] = useState(null);
+    const [isMuted, setIsMuted] = useState(false);
+    const [playGanador, { stop: stopGanador }] = useSound(Ganador);
+    const [playPerdedor, { stop: stopPerdedor }] = useSound(Perdedor);
+    const [playEmpate, { stop: stopEmpate }] = useSound(Empate);
     const [msgOutput, setMsgOutput] = useState("");
     const [state, setState] = useState(false);
 
@@ -35,6 +43,9 @@ const SinglePlayer = () => {
         setCounterComp(0);
         setUserSelection(null);
         setCompSelection(null);
+        stopGanador();
+        stopPerdedor();
+        stopEmpate();
         setMsgOutput("");
         setState(false);
     };
@@ -44,6 +55,9 @@ const SinglePlayer = () => {
         setCounterComp(0);
         setUserSelection(null);
         setCompSelection(null);
+        stopGanador();
+        stopPerdedor();
+        stopEmpate();
         setMsgOutput("");
         if (counterUser === 3) {
             setCounterTotalUser(counterTotalUser + 1)
@@ -61,15 +75,61 @@ const SinglePlayer = () => {
         setState(true);
     };
 
+    const changeIsMuted = () => {
+        if (isMuted) {
+            setIsMuted(false);
+        } else {
+            setIsMuted(true);
+            stopGanador();
+            stopPerdedor();
+            stopEmpate();
+        }
+    };
+
+    const iconSound = () => {
+        if (isMuted) {
+          return (
+            <>
+              <div className='icon'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-volume-mute-fill" viewBox="0 0 16 16" onClick={ () => changeIsMuted()}>
+                  <path d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zm7.137 2.096a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z"/>
+                </svg>
+              </div>
+            </>
+          );
+        } else {
+          return (
+            <>
+              <div className='icon'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-volume-up-fill" viewBox="0 0 16 16" onClick={ () => changeIsMuted()}>
+                  <path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/>
+                  <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/>
+                  <path d="M8.707 11.182A4.486 4.486 0 0 0 10.025 8a4.486 4.486 0 0 0-1.318-3.182L8 5.525A3.489 3.489 0 0 1 9.025 8 3.49 3.49 0 0 1 8 10.475l.707.707zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06z"/>
+                </svg>
+              </div>
+            </>
+          );
+        }
+    };
+
     const compareAndSetStates = (selection1, selection2) => {
         if (selection1.win.includes(selection2.name)) {
             setCounterUser(counterUser + 1);
             setMsgOutput("Ganaste el punto");
+            if (!isMuted){
+                playGanador();
+            }
         } else if ( selection1.name === selection2.name) {
             setMsgOutput("Han empatado");
+            if (!isMuted){
+                playEmpate();
+            }
         } else {
             setCounterComp(counterComp + 1);
             setMsgOutput("Perdiste el punto");
+            if (!isMuted){
+                playPerdedor();
+            }
         }
     };
 
@@ -192,6 +252,7 @@ const SinglePlayer = () => {
                     <div className='col-lg-12 col-md-12 col-sm-12 col-xs-12 navbar-bottom'>
                         <a onClick={goBack} className="btn-gp">SALIR</a>
                         {renderByState()}
+                        {iconSound()}
                     </div> 
                 </div> 
                 <div className='col-lg-12 col-md-12 col-sm-12 col-xs-12 content-game'>
