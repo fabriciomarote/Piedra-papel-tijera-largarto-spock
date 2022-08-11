d43c6000def3ba5b6b795856559f1f2fb8340ae8
fabriciomarote <fabrii.cai93@gmail.com>
2022-08-10 22:23:14 -0300

    cambios en mainPage

--- src/components/MultiPlayer.jsx
+++ src/components/MultiPlayer.jsx
@@ -4,7 +4,6 @@ import { selections } from './Selections';
 import { FaArrowDown } from "react-icons/fa";
 import { useSound }  from 'use-sound';
 import Ganador from '../media/ganador.mp3'
-import Perdedor from '../media/derrota2.mp3'
 import Empate from '../media/empate.mp3'
 import Boton from '../media/botones3.mp3'
 import SelectBoton from '../media/botones2.mp3'
@@ -24,9 +23,6 @@ const MultiPlayer = () => {
     const [player2Selection, setPlayer2Selection] = useState(null);
     const [isMuted, setIsMuted] = useState(false);
     const [playGanador, { stop: stopGanador }] = useSound(Ganador, {
-        volume: 0.2,
-    }); 
-    const [playPerdedor, { stop: stopPerdedor }] = useSound(Perdedor, {
         volume: 0.2,
     }); 
     const [playEmpate, { stop: stopEmpate }] = useSound(Empate, {
@@ -79,7 +75,6 @@ const MultiPlayer = () => {
         setPlayer1Selection(null);
         setPlayer2Selection(null);
         stopGanador();
-        stopPerdedor();
         stopEmpate();
         setMsgOutput("");
         setButtonsBlocked(false);
@@ -134,34 +129,33 @@ const MultiPlayer = () => {
         } else {
             setIsMuted(true);
             stopGanador();
-            stopPerdedor();
             stopEmpate();
         }
     };
 
     const iconSound = () => {
         if (isMuted) {
-          return (
-            <>
-              <div className='icon'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-volume-mute-fill" viewBox="0 0 16 16" onClick={ () => changeIsMuted()}>
-                  <path d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zm7.137 2.096a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z"/>
-                </svg>
-              </div>
-            </>
-          );
+            return (
+                <>
+                    <div className='icon'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-volume-mute-fill" viewBox="0 0 16 16" onClick={ () => changeIsMuted()}>
+                        <path d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zm7.137 2.096a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z"/>
+                        </svg>
+                    </div>
+                </>
+            );
         } else {
-          return (
-            <>
-              <div className='icon'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-volume-up-fill" viewBox="0 0 16 16" onClick={ () => changeIsMuted()}>
-                  <path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/>
-                  <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/>
-                  <path d="M8.707 11.182A4.486 4.486 0 0 0 10.025 8a4.486 4.486 0 0 0-1.318-3.182L8 5.525A3.489 3.489 0 0 1 9.025 8 3.49 3.49 0 0 1 8 10.475l.707.707zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06z"/>
-                </svg>
-              </div>
-            </>
-          );
+            return (
+                <>
+                    <div className='icon'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-volume-up-fill" viewBox="0 0 16 16" onClick={ () => changeIsMuted()}>
+                        <path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/>
+                        <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/>
+                        <path d="M8.707 11.182A4.486 4.486 0 0 0 10.025 8a4.486 4.486 0 0 0-1.318-3.182L8 5.525A3.489 3.489 0 0 1 9.025 8 3.49 3.49 0 0 1 8 10.475l.707.707zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06z"/>
+                        </svg>
+                    </div>
+                </>
+            );
         }
     };
 
@@ -191,7 +185,7 @@ const MultiPlayer = () => {
 
     const renderStarting = () => {
         if (stateOn) {
-             if (counterPlayer1 === 3) {
+            if (counterPlayer1 === 3) {
                 if (!isMuted) {
                     playGanador();
                 }
--- src/components/SinglePlayer.jsx
+++ src/components/SinglePlayer.jsx
@@ -119,35 +119,34 @@ const SinglePlayer = () => {
 
     const iconSound = () => {
         if (isMuted) {
-          return (
-            <>
-              <div className='icon'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-volume-mute-fill" viewBox="0 0 16 16" onClick={ () => changeIsMuted()}>
-                  <path d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zm7.137 2.096a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z"/>
-                </svg>
-              </div>
-            </>
-          );
+            return (
+                <>
+                    <div className='icon'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-volume-mute-fill" viewBox="0 0 16 16" onClick={ () => changeIsMuted()}>
+                        <path d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zm7.137 2.096a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z"/>
+                        </svg>
+                    </div>
+                </>
+            );
         } else {
-          return (
-            <>
-              <div className='icon'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-volume-up-fill" viewBox="0 0 16 16" onClick={ () => changeIsMuted()}>
-                  <path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/>
-                  <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/>
-                  <path d="M8.707 11.182A4.486 4.486 0 0 0 10.025 8a4.486 4.486 0 0 0-1.318-3.182L8 5.525A3.489 3.489 0 0 1 9.025 8 3.49 3.49 0 0 1 8 10.475l.707.707zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06z"/>
-                </svg>
-              </div>
-            </>
-          );
+            return (
+                <>
+                    <div className='icon'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-volume-up-fill" viewBox="0 0 16 16" onClick={ () => changeIsMuted()}>
+                        <path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/>
+                        <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/>
+                        <path d="M8.707 11.182A4.486 4.486 0 0 0 10.025 8a4.486 4.486 0 0 0-1.318-3.182L8 5.525A3.489 3.489 0 0 1 9.025 8 3.49 3.49 0 0 1 8 10.475l.707.707zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06z"/>
+                        </svg>
+                    </div>
+                </>
+            );
         }
     };
 
     const compareAndSetStates = (selection1, selection2) => {
         if (selection1.win.includes(selection2.name)) {
             setCounterUser(counterUser + 1);
-            setMsgOutput("Ganaste el punto");
-            
+            setMsgOutput("Ganaste el punto");     
         } else if ( selection1.name === selection2.name) {
             setMsgOutput("Han empatado");
             if (!isMuted){
@@ -155,14 +154,13 @@ const SinglePlayer = () => {
             }
         } else {
             setCounterComp(counterComp + 1);
-            setMsgOutput("Perdiste el punto");
-            
+            setMsgOutput("Perdiste el punto");   
         }
     };
 
     const renderStarting = () => {
         if (state) {
-             if (counterUser === 3) {
+            if (counterUser === 3) {
                 if (!isMuted){
                     playGanador();
                 }
@@ -295,7 +293,6 @@ const SinglePlayer = () => {
                     <Footer/> 
                 </div>   
             </div>
-            
         </>
     );
 };
